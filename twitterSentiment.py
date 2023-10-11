4ddcda6a8fe7feece15017db0d9ea0b9fec337e7
Pulkit <123824584+PulkitRawat@users.noreply.github.com>
2023-10-11 17:39:05 +0530

    cleaning data

--- twitterSentiment.py
+++ twitterSentiment.py
@@ -2,4 +2,18 @@ import pandas as pd
 
 sentiment = ['positive', 'neutral', 'negative']
 df = pd.read_csv('Twitter_Data.csv')
-print(df.head())
+# print(df.head())
+
+df = df.drop_duplicates()
+df = df.dropna(subset=['clean_text', 'category'])
+
+# # print(df.head())
+# duplicates = df.duplicated()
+# df_duplicate = df[duplicates]
+# print('duplicate rows')
+# print(df_duplicate)
+
+# print('missing values rows')
+# missing_values = df.isna()
+# rows_with_missing = missing_values.any(axis=1)
+# print(df[rows_with_missing])    
